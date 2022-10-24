c4e8a8ab86f0942d7383fd5f18dc7fd90ea4a0e8
moustafaMahran <moustafa.mahran208@gmail.com>
2022-10-24 04:25:58 +0200

    adding body parser

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path")
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.json())
 app.use(require("./routes/main"));
 const dbo = require("./db/conn");
  
